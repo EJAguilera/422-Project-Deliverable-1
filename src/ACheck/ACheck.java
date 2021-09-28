4fb410fcfb3727c29a794fd1d1b4b27a70dc95b8
eaguiler <elisha.aguilera@wsu.edu>
2021-09-27 18:20:38 -0700

    Extended ACheck to implement AbstractCheck

--- src/ACheck/ACheck.java
+++ src/ACheck/ACheck.java
@@ -1,5 +1,25 @@
 package ACheck;
 
-public class ACheck {
+import com.puppycrawl.tools.checkstyle.api.*;
+
+public class ACheck extends AbstractCheck {
+
+	@Override
+	public int[] getDefaultTokens() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int[] getAcceptableTokens() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int[] getRequiredTokens() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	// Stub to be filled
 }
