a5cda4ab28e0f82a97f32e5274f1a99264d5e72d
eaguiler <elisha.aguilera@wsu.edu>
2021-09-27 19:22:58 -0700

    Implemented `BCheck.java` number-of-comments code

--- src/BCheck/BCheck.java
+++ src/BCheck/BCheck.java
@@ -1,5 +1,44 @@
 package BCheck;
 
-public class BCheck {
-	// stub to extend
+import com.puppycrawl.tools.checkstyle.api.*;
+
+//A limited implementation of the number of comments check.
+public class BCheck extends AbstractCheck {
+	
+	// Number of comments
+	private int numComments = 0;
+
+	@Override
+	public int[] getDefaultTokens() {
+
+		// Gets all tokens considered 'comments' by Checkstyle.
+		return new int[] {
+			TokenTypes.SINGLE_LINE_COMMENT, TokenTypes.BLOCK_COMMENT_END
+		};
+	}
+	
+	@Override
+	public int[] getAcceptableTokens() {
+
+		// Gets all tokens considered 'comments' by Checkstyle.
+		return new int[] {
+			TokenTypes.SINGLE_LINE_COMMENT, TokenTypes.BLOCK_COMMENT_END
+		};
+	}
+	
+	@Override
+	public int[] getRequiredTokens() {
+		// Stubbed for compliance
+		return new int[0];
+	}
+	
+	@Override
+	public void visitToken(DetailAST ast) {
+		
+		// Increment number of comments
+		numComments += 1;
+		
+		// Output incremented and logged to console
+		log(ast.getLineNo(), String.format("EJA ACHECK: New comment found! Integer type: %s! Number of comments now: %s!", ast.getType(), numComments));
+	}
 }
